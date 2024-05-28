0710c3811129d57722ff8118966960fabd07a99d
Sureshsk3 <sureshkumarq7@gmail.com>
2024-05-28 19:22:34 +0530

    new files added

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Nav from "./Components/Nav";
+import Header from "./Components/Header";
+import FirstSection from "./Components/FirstSection";
+import SecoundSection from "./Components/SecoundSection";
+import ThirdSection from "./Components/ThirdSection";
+import FourthSection from "./Components/FourthSection";
+import Footer from "./Components/Footer";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Header />
+      <FirstSection />
+      <SecoundSection />
+      <ThirdSection />
+      <FourthSection />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
